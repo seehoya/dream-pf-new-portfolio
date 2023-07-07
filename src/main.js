94e0f5dd9c36ffed9743e96ae0c8e1ba05cb37ee
siho <hellohoya@gmail.com>
2023-07-08 06:34:05 +0900

    JS-Home

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 'use strict';
 
 const header = document.querySelector('.header');
-const headerHeight = header.getBoundingClientRect().height;
+const headerHeight = header.offsetHeight;
 document.addEventListener('scroll', () => {
   if (window.scrollY > headerHeight) {
     header.classList.add('header--dark');
   } else {
     header.classList.remove('header--dark');
   }
+});
+
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+document.addEventListener('scroll', () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
 });
