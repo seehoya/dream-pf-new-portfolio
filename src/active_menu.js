5bbfb0930b2f02b7b8689276df32f7fc507cb337
siho <hellohoya@gmail.com>
2023-07-10 22:14:37 +0900

    Observer-part2

--- src/active_menu.js
+++ src/active_menu.js
@@ -13,8 +13,12 @@ const navItems = sectionIds.map((id) =>
   document.querySelector(`[href="${id}"]`)
 );
 const visibleSections = sectionIds.map(() => false);
+let activeNavItem = navItems[0];
 
-const options = {};
+const options = {
+  rootMargin: '-20% 0px 0px 0px',
+  threshold: [0, 0.98],
+};
 const observer = new IntersectionObserver(observerCallback, options);
 sections.forEach((section) => observer.observe(section));
 
@@ -26,18 +30,25 @@ function observerCallback(entries) {
     selectLastOne =
       index === sectionIds.length - 1 &&
       entry.isIntersecting &&
-      entry.intersectionRatio >= 0.99;
+      entry.intersectionRatio >= 0.95;
   });
-  console.log(visibleSections);
-  console.log('무조건 라스트 섹션!!', selectLastOne);
 
   const navIndex = selectLastOne
-    ? sectionIds.lenngth - 1
+    ? sectionIds.length - 1
     : findFirstIntersecting(visibleSections);
-  console.log(sectionIds[navIndex]);
+
+  selectNavItem(navIndex);
 }
 
 function findFirstIntersecting(intersections) {
   const index = intersections.indexOf(true);
   return index >= 0 ? index : 0;
+}
+
+function selectNavItem(index) {
+  const navItem = navItems[index];
+  if (!navItem) return;
+  activeNavItem.classList.remove('active');
+  activeNavItem = navItem;
+  activeNavItem.classList.add('active');
 }
